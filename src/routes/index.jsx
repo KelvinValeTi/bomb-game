4d3bc6534cd058682a657b0d1afb5a3e0d066b80
kelvin <kelvinvaleti@gmail.com>
2024-03-17 16:50:55 -0300

    finalização de play alone style apenas

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -3,6 +3,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import Start from "../pages/Start/index";
 import Rules from "../pages/Rules";
+import PlayAlone from "../pages/PlayAlone";
 
 const Stack = createNativeStackNavigator();
 
@@ -11,8 +12,8 @@ export default function Routes(){
         <NavigationContainer>
             <Stack.Navigator screenOptions={{headerShown:false}}>
                 <Stack.Screen name="Start" component={Start} />
-                
                 <Stack.Screen name="Rules" component={Rules} />
+                <Stack.Screen name="PlayAlone" component={PlayAlone} />
                 
             </Stack.Navigator>
         </NavigationContainer>
