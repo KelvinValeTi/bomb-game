6dc6bc65ea69775ea735b8e9a45a3d25593e02f1
kelvin <kelvinvaleti@gmail.com>
2024-03-16 21:13:58 -0300

    rules page

--- App.js
+++ App.js
@@ -2,8 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Start from './src/pages/Start/index.jsx';
+import Rules from './src/pages/Rules/index.jsx';
 
 
 export default function App() {
-  return <Start></Start>
+  return <>
+      <StatusBar color="light" />
+    <Rules></Rules>
+  </>
 }
