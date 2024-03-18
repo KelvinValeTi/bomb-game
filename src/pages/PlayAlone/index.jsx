a851646672e7ca0108caa57f55d307a625cdef68
kelvin <kelvinvaleti@gmail.com>
2024-03-18 12:02:13 -0300

    criação do serviço de inicio do jogo + link com o jogo solo

--- src/pages/PlayAlone/index.jsx
+++ src/pages/PlayAlone/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { Alert } from "react-native";
 import { Container, TextTimer, Timer, TipContainer, TipText, TipTitle, Title } from "./styles";
 import { ImageBackground } from "react-native";
@@ -7,16 +7,79 @@ import { useNavigation } from "@react-navigation/native";
 import bombImg from "../../assets/bomba.png";
 import PasswordInput from "../../components/PasswordInput";
 import ButtonComponent from "../../components/Buttons/index"
+import BombService from "../../services/BombApp/index"
+import api from "../../services/api/api"
 
 export default function PlayAlone(){
     const navigation = useNavigation();
+
+    const [started, setStarted] = useState(false);
+    const [pin, setPin] = useState(["","",""]);
+
+    const [hours, setHours] = useState("00");
+    const [minutes, setMinutes] = useState("03");
+    const [seconds, setSeconds] = useState("00");
     
+    const [question, setQuestion] = useState("");
+    const [answer, setAnswer] = useState("");
+    const [intervalId, setIntervalId] = useState();
+
     function handleStartGame(){
-        Alert.alert("Jogo Iniciado");
+        BombService.bombStartGame({setStarted, hours, minutes, seconds});
     }
+
+    async function fetchQuestion(){
+        const randomNumber = Math.floor(Math.random()* 6 + 1);
+
+        const {data} = await api.get(`questions/${randomNumber}`);
+
+        setQuestion(data?.pergunta);
+        setAnswer(data?.resp);
+    }
+    useEffect(()=>{
+        fetchQuestion();
+    },[]);
+
+   
+
+    function handleStartBomb(){
+        const diffTime = BombService.getDiffTime({hours, minutes, seconds});
+
+        BombService.startCountDown({
+            setSeconds,
+            setMinutes,
+            setHours,
+            setStarted,
+            diffTime,
+            setIntervalId,
+            intervalId,
+            navigation
+        });
+    }
+
+    function handleDisarmBomb(){
+        BombService.disarmBomb({
+            setStarted, 
+            answer, 
+            navigation, 
+            pin, 
+            setPin, 
+            intervalId
+        });
+    }
+    function handleGiveUp(){
+        BombService.giveUpGame({intervalId, navigation});
+    }
+
     function handleNavToStart(){
         navigation.navigate("Start");
     }
+
+    useEffect(()=>{
+        if(started){
+            handleStartBomb();
+        }
+    },[started]);
     
     return(
         <Container>
@@ -32,18 +95,32 @@ export default function PlayAlone(){
                 }}
             >
                 <Timer>
-                    <TextTimer>00:00:00</TextTimer>
+                    <TextTimer>{hours}:{minutes}:{seconds}</TextTimer>
                 </Timer>
             </ImageBackground>
-            <TipContainer>
-                <TipTitle>Sua dica:</TipTitle>
-                <TipText>Sua dica vai estar aqui!</TipText>
-            </TipContainer>
 
-            <PasswordInput></PasswordInput>
-            <ButtonComponent buttonText="Iniciar" handlePress={handleStartGame} />
-            <ButtonComponent buttonText="Página Inicial" handlePress={handleNavToStart} /> 
+            {!started ? null : (
+                <TipContainer>
+                    <TipTitle>Sua dica:</TipTitle>
+                    <TipText>{question}</TipText>
+                    <TipText>{answer}</TipText>
+                </TipContainer>
+            )}
             
+
+            <PasswordInput pin={pin} setPin={setPin} started={started}/>
+
+            {!started ? (
+                <>
+                    <ButtonComponent buttonText="Iniciar" handlePress={handleStartGame} />
+                    <ButtonComponent buttonText="Página Inicial" handlePress={handleNavToStart} />
+                </>
+            ) : (
+                <>
+                    <ButtonComponent buttonText="Desarmar" handlePress={handleDisarmBomb} />
+                    <ButtonComponent buttonText="Desistir" handlePress={handleGiveUp} /> 
+                </>
+            )}
         </Container>
     );
 }
