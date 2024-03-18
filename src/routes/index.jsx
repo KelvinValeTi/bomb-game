c9e10eb2f1f3605abdd7ad392956dd913f1983c0
kelvin <kelvinvaleti@gmail.com>
2024-03-18 07:38:36 -0300

    bomba desarmada page

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -5,6 +5,7 @@ import Start from "../pages/Start/index";
 import Rules from "../pages/Rules";
 import PlayAlone from "../pages/PlayAlone";
 import PlayTogether from "../pages/PlayTogether";
+import Disarmed from "../pages/Disarmed";
 
 const Stack = createNativeStackNavigator();
 
@@ -12,6 +13,10 @@ export default function Routes(){
     return(
         <NavigationContainer>
             <Stack.Navigator screenOptions={{headerShown:false}}>
+
+
+                <Stack.Screen name="Disarmed" component={Disarmed} />
+
                 <Stack.Screen name="Start" component={Start} />
                 <Stack.Screen name="Rules" component={Rules} />
                 <Stack.Screen name="PlayAlone" component={PlayAlone} />
